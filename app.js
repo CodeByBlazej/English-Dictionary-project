6922867c5ec4102676b7c7f4e2c62a2f328bc147
CodeByBlazej <codebyblazej@gmail.com>
2023-12-16 22:08:48 +0000

    added button as extra task

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const inputEl = document.getElementById('input');
 const infoTextEl = document.getElementById('info-text');
 const meaningContainerEl = document.getElementById('meaning-container');
+const titleEl = document.getElementById('title');
+const meaningEl = document.getElementById('meaning');
+const audioEl = document.getElementById('audio');
+const btnEl = document.getElementById('btn');
 
 async function fetchAPI(word) {
   try {
@@ -9,16 +13,35 @@ async function fetchAPI(word) {
     infoTextEl.innerText = `Searching the meaning of "${word}"`
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
     const result = await fetch(url).then((res) => res.json());
-    infoTextEl.style.display = 'none';
-    meaningContainerEl.style.display = 'block';
+
+    if (result.title) {
+      meaningContainerEl.style.display = 'block';
+      infoTextEl.style.display = 'none';
+      titleEl.innerText = word;
+      meaningEl.innerText = 'N/A'
+      audioEl.style.display = 'none'
+    } else {
+      infoTextEl.style.display = 'none';
+      meaningContainerEl.style.display = 'block';
+      audioEl.style.display = 'inline-flex';
+      titleEl.innerText = result[0].word;
+      meaningEl.innerText = result[0].meanings[0].definitions[0].definition;
+      audioEl.src = result[0].phonetics[0].audio;
+    }
+    
   } catch (error) {
     console.log(error);
+    infoTextEl.innerText = `an error happened, try again later...`
   }
 }
 
 
 inputEl.addEventListener('keyup', (e) => {
   if (e.target.value && e.key === 'Enter') {
-    fetchAPI(e.target.value)
+    fetchAPI(e.target.value);
   }
-})
+  });
+
+  btnEl.addEventListener('click', (e) => {
+    fetchAPI(inputEl.value);
+  })
